d1b8c85ed41155d8c56914fbbf5a11099c20e0bc
FlaVeez <flavianbadau@aol.com>
2026-03-29 22:41:17 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY
-
+ 
+const url = 'https://ijcudmweioyetpttzzne.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqY3VkbXdlaW95ZXRwdHR6em5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NjU3NDAsImV4cCI6MjA5MDE0MTc0MH0.Rzn8DZ3fQT3iXGjz0Pjb9QeQV-e0A1qilKPqqPbC1Wc'
+ 
 export const supabase = createClient(url, key)
